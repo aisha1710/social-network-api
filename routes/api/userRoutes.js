ccedb2a3dde21d486852026bd7d80e3aa69a309e
aisha <faisha879@gmail.com>
2022-12-05 17:01:18 +0000

    user routes completed

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+
+const {
+  getAllUser,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/userController");
+
+router.route("/").get(getAllUser).post(createUser);
+
+router.route("/: userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+module.exports = router;
